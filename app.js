ee598d4276ef506436092e4434f87dff75488dc0
rmsvictor <67174439+rmsvictor@users.noreply.github.com>
2020-07-15 17:35:04 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+///Specifies a static folder where static file are
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/" , function(req, res){
+   res.sendFile(__dirname + "/signup.html");
+})
+
+
+app.post("/", function(req, res){
+   const firstName = req.body.fName;
+   const lastName = req.body.lName;
+   const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+   const jsonData = JSON.stringify(data);
+
+   const url = "https://us10.api.mailchimp.com/3.0/lists/734047fd05";
+
+   const options = {
+     method: "POST",
+     auth: 'anystring:d599d363463c8b01ce28488730bde4d4-us10'
+   }
+
+    const request = https.request(url, options, function(response){
+
+if (response.statusCode === 200){
+  res.sendFile(__dirname + "/success.html");
+} else {
+  res.sendFile(__dirname + "/failure.html");
+}
+
+
+
+     response.on("data", function(data){
+       console.log(JSON.parse(data));
+     })
+   })
+request.write(jsonData);
+request.end();
+
+})
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+   console.log("server running on port 3000");
+})
+
+
+// API Key
+// d599d363463c8b01ce28488730bde4d4-us10
+
+// List ID
+// 734047fd05
+
+//HEROKU link , page posted
+// https://rocky-citadel-51166.herokuapp.com/
